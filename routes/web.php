ec97c1c5598d1ad9e1b071d72e2601cfec40935b
navdbaloch <naveed@octraves.com>
2021-02-03 19:49:16 +0500

    - reverted gzip for now as creating issue on server

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,12 @@ $router->get('/', function () use ($router) {
 $router->get('/profile/{platform}/{relativeURL}', 'ProfileController@get');
 $router->get('/profile/get-from-cache', 'ProfileController@getFromCache');
 // search profiles
-// $router->get('/profiles', 'ProfileController@index');
-$router->get('/profiles', [
-    'middleware' => 'gzip',
-    // 'as' => 'data',
-    'uses'=>'ProfileController@index'
-]);
+$router->get('/profiles', 'ProfileController@index');
+// $router->get('/profiles', [
+//     'middleware' => 'gzip',
+//     // 'as' => 'data',
+//     'uses'=>'ProfileController@index'
+// ]);
 // count profiles for a search
 $router->get('/profiles/count', 'ProfileController@count');
 // total profiles count by each platform
