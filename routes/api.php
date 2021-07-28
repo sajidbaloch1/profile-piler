b2839f51db491898ddf1da72c4571a405b80ad00
Naveed <mnaveedbaloch@gmail.com>
2021-07-28 16:40:10 +0500

    applied Cors to api route group

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\API\KeywordApiController;
 use App\Http\Controllers\API\ProfileApiController;
 use App\Http\Controllers\API\SocialEntityController;
 use App\Http\Controllers\API\SocialFeedController;
-use App\Http\Controllers\CuratedListController;
 use App\Http\Controllers\LookupController;
 use Illuminate\Http\Request;
 
@@ -19,31 +18,22 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-
 $router->get('/', function () use ($router) {
     return ['appVersion' => $router->app->version()];
 });
 
-// Get profile details
+
 $router->get('/profile/{platform}/{relativeURL}', [ProfileApiController::class, 'get']);
 $router->get('/profile/get-from-cache', [ProfileApiController::class, 'getFromCache']);
-// search profiles
+
 $router->get('/profiles', [ProfileApiController::class, 'index']);
-// $router->get('/profiles', [
-//     'middleware' => 'gzip',
-//     // 'as' => 'data',
-//     'uses'=>'ProfileApiController@index'
-// ]);
-// count profiles for a search
 $router->get('/profiles/count', [ProfileApiController::class, 'count']);
-// total profiles count by each platform
 $router->get('/platform-stats', [ProfileApiController::class, 'platformStats']);
-// categories list
+
 $router->get('/categories', [KeywordApiController::class, 'index']);
-// keyword list
+
 $router->get('/keywords', [KeywordApiController::class, 'keywords']);
-// auto-completion
+
 $router->get('/auto-complete', [LookupController::class, 'index']);
 
 // log front-end errors
