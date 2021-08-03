71e4e53f41f4cca7730a01c6ba81bd05ee817e2c
Naveed Baloch <naveed@octraves.com>
2021-08-03 12:21:01 +0500

    test ci/cd

--- app/Http/Controllers/API/SocialFeedController.php
+++ app/Http/Controllers/API/SocialFeedController.php
@@ -10,6 +10,7 @@ class SocialFeedController extends Controller
 {
     public function feed($platform, Request $request)
     {
+        return ['success' => false, 'test'=>'test'];
         return (new FeedApiClient)->get("/social-feed/$platform", $request->all());
     }
 }
