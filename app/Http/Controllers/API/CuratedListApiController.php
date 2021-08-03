13074f9a3115cab6ee38e28b917bcd1c3c7f0df1
Naveed <mnaveedbaloch@gmail.com>
2021-08-03 18:05:40 +0500

    tag filter on lists

--- app/Http/Controllers/API/CuratedListApiController.php
+++ app/Http/Controllers/API/CuratedListApiController.php
@@ -6,16 +6,22 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\CuratedListCollection;
 use App\Http\Resources\CuratedListResource;
 use App\Models\CuratedList;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class CuratedListApiController extends Controller
 {
     public function index(Request $request)
     {
-        $list = CuratedList::with('profiles', 'tags')
-            ->where('is_active', true)
-            ->paginate(2);
-        return new CuratedListCollection($list);
+        $listQuery = CuratedList::with('profiles', 'tags')
+            ->where('is_active', true);
+        if ($request->filled('tag')) {
+            $listQuery = $listQuery->whereHas('tags', function (Builder $query) use ($request) {
+                $query->where('name', $request->get('tag'));
+            });
+        }
+        $lists = $listQuery->paginate();
+        return new CuratedListCollection($lists);
     }
 
     public function show($seo_url)
