805c27a5a20cab36972eadd6ea040f679c359baa
SHAHZAD-PC <shahzaddbaloch9919@gmail.com>
2022-03-29 21:01:04 +0500

    sort data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -23,23 +23,23 @@ class DashboardController extends Controller
     public function getElasticSearchLogs()
     {
         $elasticSearch = ElasticSearchLog::select('query_time_ms', 'created_at')
-        ->where('created_at','>=', date('Y-m-d', strtotime('-1 week')))
-        ->get()->map(function ($e) {
-            return [
-                "created_at" => date('Y-m-d', strtotime($e->created_at)),
-                "query_ms" => $e->query_time_ms
-            ];
-        })->collect()->groupBy('created_at');
-        $favgQueryMs = [];
-        foreach($elasticSearch as $k => $e){
-            $avgQueryMs = $e->map(function($v){
+            ->where('created_at', '>=', date('Y-m-d', strtotime('-7 days')))
+            ->get()->map(function ($e) {
+                return [
+                    "created_at" => date('Y-m-d', strtotime($e->created_at)),
+                    "query_ms" => $e->query_time_ms
+                ];
+            })->collect()->sortBy('created_at')->groupBy('created_at');
+        $fAvgQueryMs = [];
+        foreach ($elasticSearch as $k => $e) {
+            $avgQueryMs = $e->map(function ($v) {
                 return $v['query_ms']++;
             });
-            $favgQueryMs[] = [
+            $fAvgQueryMs[] = [
                 "label" => date('M d', strtotime($k)),
-                "value" => number_format($avgQueryMs->collect()->avg(),2)
+                "value" => number_format($avgQueryMs->collect()->avg(), 2)
             ];
         };
-        return $favgQueryMs;
+        return $fAvgQueryMs;
     }
 }
