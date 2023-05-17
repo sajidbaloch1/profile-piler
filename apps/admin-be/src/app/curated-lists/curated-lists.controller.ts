7eca1a6e8c0e2da3f04fc0764462d1a2fb5a3a3e
DESKTOP-ADTNMMQ\Sajid Pc <kinglovers109@gmail.com>
2023-05-17 18:59:59 +0500

    highlighted issue

--- apps/admin-be/src/app/curated-lists/curated-lists.controller.ts
+++ apps/admin-be/src/app/curated-lists/curated-lists.controller.ts
@@ -14,10 +14,7 @@ import { CuratedListsService } from './curated-lists.service';
 export class CuratedListsController {
   constructor(private readonly curatedService: CuratedListsService) {}
 
-  // @Get()
-  // findAll() {
-  //   return this.curatedService.getAllCuratedLists();
-  // }
+ 
   @Get()
   findAll() {
     return this.curatedService.getAllCuratedLists();
@@ -33,12 +30,6 @@ export class CuratedListsController {
     const newRow = await this.curatedService.createRow(data);
     return newRow;
   }
-
-  // @Put(':id')
-  // async updateRowById(@Param('id') id: string, @Body() updatedData: any) {
-  //   const updatedRow = await this.curatedService.updateRowById(id, updatedData);
-  //   return updatedRow;
-  // }
 
   @Patch(':id')
   async isActiveRecord(@Param('id') id: number, @Body() isActive: boolean) {
